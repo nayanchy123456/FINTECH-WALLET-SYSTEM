0d80657b11fce7a001cc08cf88a5a944efd3ed28
nayanchy123456 <nayanchy2004@gmail.com>
2026-05-23 16:36:18 +0545

    initial commit " project setup is done "

---
+++ wallet/src/test/java/com/paymentprocessing/wallet/WalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymentprocessing.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
